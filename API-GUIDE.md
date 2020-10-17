15ba1f52aac7a108dd68721f99cce28feacd310f
James Scolamieri <james.scolamieri@gmail.com>
2020-10-17 12:58:44 -0700

    imported changes to API file and updated property seed script

--- API-GUIDE.md
+++ API-GUIDE.md
@@ -2,8 +2,12 @@
 
 
 
-### 12 random listings
+### at a given property listings, gets 12 random property listings for more-places component
   * GET `/api/more_places`
+
+
+UPDATE ROUTES TO USE STANDARD CONVENTION (ID PER PAGE)
+  * GET `/api/listings/:id/more-places`
 
 **Path Parameters:**
   * none ( get all limit 12 (randomized) )
@@ -35,8 +39,9 @@
 
 
 
-### Get saved lists
+### Gets a user's saved property collections
   * GET `/api/collections`
+  * GET `/api/user/:id/collections`
 
 **Path parameters:**
   * none (get all)
@@ -53,14 +58,23 @@
         "photoUrl": "https://source.unsplash.com/480x480/?home&sig=0.8394293199360257",
         "count": 2,
         "time": "Any time"
+    },
+    {
+        "_id": "5f873d26c45c11154b4f50b0",
+        "name": "mylist",
+        "photoUrl": "https://source.unsplash.com/480x480/?home&sig=0.8394293199360257",
+        "count": 2,
+        "time": "Any time",
+        "saved_properties": "['456217652', '561643219']"
     }
 ]
 ```
 
 
 
-### Get specific collection by houseId
+### FLAGGED FOR DEPRECATION - When removing a property from saved lists, gets the saved list name from a property id
   * GET `/api/properties/collections`
+  * GET `/api/properties/:id/collections`
 
 **Path parameters:**
   * query `/hcho1?houseId=1`
@@ -78,8 +92,9 @@
 
 
 
-### Add list
+### Adds a new collection to db
   * POST `/api/collections`
+  * POST `/api/user/:id/collections`
 
 **Success Status Code:** `201`
 
@@ -95,7 +110,11 @@
 
 
 ### update the saved props of a listing and count of collection when clicked
+
+### save and unsave a property to a list (or lists)
+
   * PATCH `/api/collections`
+  * PATCH `/api/user/:id/collections`
 
 **Path Parameters:**
   * none, uses req.body
@@ -116,9 +135,10 @@
 
 ### Delete all collections
   * DELETE `/api/collections`
+  * DELETE `/api/user/:id/collections/:name`
 
 **Path Parameters:**
-  * query `/?name=String`
+  * REMOVE, use request parameters instead of: query `/?name=String`
 
 **Success Status Code:** `204`
 
--- bigSeed/propertySeed.js
+++ bigSeed/propertySeed.js
@@ -8,10 +8,11 @@ const csvWriter = require('csv-write-stream');
 
 const writer = csvWriter();
 
-const partition_ids = [111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130];
+// eslint-disable-next-line max-len
+const partitionIds = [111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130];
+const description = ['Central Location!', 'Comfy and Quaint!', 'Very quiet neighborhood', '100% Private!', 'Ultra Luxury Room', '420 Friendly Stay!', 'Cozy and Comfortable', 'Wake up to spectacular views', 'Panoramic Views', 'Spacious', 'Exquisite and Close to everything!', 'Stunning Views', 'Lovers Paradise', 'Minimalist Dream', 'Beautiful Midcentury Oasis', 'Contemporary Dream', 'Earthy Modernist Home', 'Large Private Area'];
 
-// const roomType = ['Entire Place', 'Shared Room', 'Private Room', 'Hotel Room', 'Entire Villa', 'Entire Guesthouse', 'Entire Cottage', 'Entire Apartment', 'Private Studio'];
-// const description = ['Central Location!', 'Comfy and Quaint!', 'Very quiet neighborhood', '100% Private!', 'Ultra Luxury Room', '420 Friendly Stay!', 'Cozy and Comfortable', 'Wake up to spectacular views', 'Panoramic Views', 'Spacious', 'Exquisite and Close to everything!', 'Stunning Views', 'Lovers Paradise', 'Minimalist Dream', 'Beautiful Midcentury Oasis', 'Contemporary Dream', 'Earthy Modernist Home', 'Large Private Area'];
+const roomType = ['Entire Place', 'Shared Room', 'Private Room', 'Hotel Room', 'Entire Villa', 'Entire Guesthouse', 'Entire Cottage', 'Entire Apartment', 'Private Studio'];
 
 const template = {
   adjective: ['Central', 'Very quiet', '100% Private', 'Exquisite', '420 Friendly', 'Earthy', 'Cozy', 'Comfortable', 'Stunning', 'Minimalist', 'Beautiful', 'Modernist', 'Private', 'Sunny', 'Shady', 'Exciting', 'Lively', 'Centrally Located', 'Colorful', 'Inexpensive', 'Pet-friendly', 'Scenic', 'Desert', 'Haunted', 'Spooky', 'Crooked', 'Gigantic'],
@@ -22,24 +23,6 @@ const template = {
 };
 
 const location = ['San Diego', 'Malibu', 'Los Angeles', 'Big Bear Lake', 'Joshua Tree', 'Palm Springs', 'Crestline', 'Newport', 'Pasadena', 'Santa Barbara', 'San Francisco', 'Granada', 'Berkeley', 'San Jose', 'Long Beach', 'Santa Monica', 'Anaheim', 'Burbank', 'Sacramento', 'Hundleby', 'Yucca Valley', 'Salisbury', 'Sutton', 'Lake Arrowhead'];
-
-// const generateData = () => {
-//   let counter = 0;
-//   writer.pipe(fs.createWriteStream('data.csv'));
-//   for (let i = 0; i < 10000000; i += 1) {
-//     writer.write({
-//       partition_id: partition_ids[Math.floor(Math.random() * 19)],
-//       property_id: counter += 1,
-//       photoUrl: `http://aws.com/${'hello'}`,
-//       descrip: 'description',
-//       rating: (Math.random() * 2) + 3,
-//       review_count: Math.floor(Math.random() * 1000),
-//       beds: Math.ceil(Math.random() * 8),
-//       price: Math.ceil(Math.random() * 8),
-//       location: location[Math.floor(Math.random() * location.length)],
-//     });
-//   }
-// };
 
 const randomDescription = () => {
   let adjIndex1 = template.adjective[Math.floor(Math.random() * template.adjective.length)];
@@ -67,8 +50,28 @@ const randomDescription = () => {
   return string;
 };
 
-// should create 2080 combinations
+const generateData = () => {
+  let counter = 0;
+  writer.pipe(fs.createWriteStream('propertySeed.csv'));
+  for (let i = 0; i < 10000000; i += 1) {
+    writer.write({
+      // partition_id: partitionIds[Math.floor(Math.random() * 19)],
+      property_id: counter += 1,
+      photoUrl: `http://aws.com/${'hello'}`,
+      descrip: 'description',
+      rating: (Math.random() * 2) + 3,
+      review_count: Math.floor(Math.random() * 1000),
+      beds: Math.ceil(Math.random() * 8),
+      price: Math.ceil(Math.random() * 8),
+      location: location[Math.floor(Math.random() * location.length)],
+    });
+  }
+};
 
-for (let i = 0; i < 100; i += 1) {
-  console.log(randomDescription());
-}
+const pipeTest = () => {
+  writer.pipe(fs.createWriteStream('pipe.test'));
+  writer.write({ hello: 'world', foo: 'bar', baz: 'taco' });
+  writer.end();
+};
+
+pipeTest();
